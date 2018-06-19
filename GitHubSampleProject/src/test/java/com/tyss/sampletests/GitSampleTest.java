edb7315bd6a4182db4665e81c187f14894e2c0c7
singhdeepa <singhdeepa9555@gmail.com>
2018-06-19 11:27:53 +0530

    First

---
+++ GitHubSampleProject/src/test/java/com/tyss/sampletests/GitSampleTest.java
@@ -0,0 +1,11 @@
+package com.tyss.sampletests;
+
+import org.testng.annotations.Test;
+
+public class GitSampleTest {
+	@Test
+	public void createUser(){
+		System.out.println("User Created");
+	}
+
+}
---
+++ GitHubSampleProject/src/test/java/com/tyss/sampletests/GitSampleTwo.java
@@ -0,0 +1,10 @@
+package com.tyss.sampletests;
+
+import org.testng.annotations.Test;
+
+public class GitSampleTwo {
+@Test
+public void editUser(){
+	System.out.println("User edited");
+}
+}
